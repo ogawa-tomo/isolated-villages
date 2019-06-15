0edf07649cd82faff83cd14a23476f3bdc565f07
ogawa-tomo <o_tomo_1990@yahoo.co.jp>
2019-06-15 21:19:45 +0900

    秘境集落に飛ばされるボタン作成のための修正

--- extract_villages_for_shindan.py
+++ extract_villages_for_shindan.py
@@ -8,9 +8,7 @@ for region in RegionSetting.get_region_list():
         region,
         1,
         1,
-        # 299,
-        4,
-        0
+        6,
     )
     result = calc_isolation.main(small_village_setting)
     villages = result.sorted_villages
@@ -19,19 +17,34 @@ for region in RegionSetting.get_region_list():
     #     print(region + "は10集落のみ抽出")
     # else:
     #     extracted_villages.extend(villages[:30])
-    extracted_villages.extend(villages[:30])
+    extracted_villages.extend(villages[:20])
 
-with open("./output/shindan.txt", "w") as f:
+# with open("./output/shindan.txt", "w") as f:
+with open("./output/button.txt", "w") as f:
     for v in extracted_villages:
+        # row = "".join([
+        #     v.pref,
+        #     v.city,
+        #     v.district,
+        #     "[BR]",
+        #     str(v.latitude_round),
+        #     ",",
+        #     str(v.longitude_round),
+        #     "\n"
+        # ])
+        # f.write(row)
+
         row = "".join([
             v.pref,
             v.city,
             v.district,
-            "[BR]",
+            "\n",
+            "http://maps.google.com/maps?q=",
             str(v.latitude_round),
             ",",
             str(v.longitude_round),
-            "\n"
+            "&t=k",
+            "\n\n"
         ])
         f.write(row)
 
